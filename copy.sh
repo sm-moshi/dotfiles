a1cd2b01171fa76677ad9b1dada809aa1cfbf9c9
Stuart Meya <smeya@3pc.de>
2022-05-10 12:22:53 +0200

    Updated: 2022-05-10 12:22:53

--- copy.sh
+++ copy.sh
@@ -2,9 +2,16 @@
 
 cp ~/.zshrc .
 echo "copied .zshrc"
+
 cp ~/.config/konsolerc .
 echo "copied konsolerc"
+
 cp ~/.config/yakuakerc .
 echo "copied yakuakerc"
+
 cp ~/.config/nvim/init.vim .config/nvim/init.vim
 echo "copied nvim-init.vim"
+
+git add .
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push
