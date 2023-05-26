e7acc3772c988eb1bbf799a6e31c1e5c4d2a2587
sm-moshi <meya@posteo.se>
2023-05-26 09:51:29 +0200

    FIX: fixed file names and paths

--- copy.sh
+++ copy.sh
@@ -6,19 +6,19 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cp ~/.antigen .antigen_linux
     cp ~/.config/konsolerc .config_linux/konsolerc_linux
     cp ~/.config/yakuakerc .config_linux/yakuakerc_linux
-    cp ~/.config/nvim/init.vim .config_linux/nvim-init_linux.vim
+    cp ~/.config/nvim/init.vim .config_linux/nvim/init.vim
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
-    cp ~/.zshrc zshrc_macos
-    cp ~/.antigen antigen_macos
-    cp ~/.config/nvim/init.vim nvim-init_macos.vim
+    cp ~/.zshrc .zshrc_macos
+    cp ~/.antigen .antigen_macos
+    cp ~/.config/nvim/init.vim .config_macos/nvim/init.vim
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
     # FreeBSD
-    cp ~/.zshrc zshrc_freebsd
-    cp ~/.antigen antigen_freebsd
+    cp ~/.zshrc .zshrc_freebsd
+    cp ~/.antigen .antigen_freebsd
     cp ~/.config/konsolerc konsolerc_freebsd
     cp ~/.config/yakuakerc yakuakerc_freebsd
-    cp ~/.config/nvim/init.vim nvim-init_freebsd.vim
+    cp ~/.config/nvim/init.vim .config_freebsd/nvim/init.vim
 else
     echo "Unsupported operating system: $OSTYPE"
     exit 1
