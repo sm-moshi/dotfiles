7bad41eba6d3d44081ea263099e1cb4eaf4530ae
sm-moshi <meya@posteo.se>
2023-06-05 09:40:34 +0200

    FIX: get OSTYPE the right way

--- copy.sh
+++ copy.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+OSTYPE=$(uname -s)
+
+if [[ "$OSTYPE" == "Linux" ]]; then
     # Linux
-    rsync -avz ~/.zshrc .zshrc_linux
-    rsync -avz ~/.antigen .antigen_linux
-    rsync -avz ~/.config/konsolerc .config_linux/konsolerc_linux
-    rsync -avz ~/.config/yakuakerc .config_linux/yakuakerc_linux
-    rsync -avz ~/.config/nvim/init.vim .config_linux/nvim/init.vim
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+    /usr/bin/rsync -avz ~/.zshrc .zshrc_linux
+    /usr/bin/rsync -avz ~/.antigen .antigen_linux
+    /usr/bin/rsync -avz ~/.config/konsolerc .config_linux/konsolerc_linux
+    /usr/bin/rsync -avz ~/.config/yakuakerc .config_linux/yakuakerc_linux
+    /usr/bin/rsync -avz ~/.config/nvim/init.vim .config_linux/nvim/init.vim
+elif [[ "$OSTYPE" == "Darwin" ]]; then
     # macOS
-    rsync -avz ~/.zshrc .zshrc_macos
-    rsync -avz ~/.antigen .antigen_macos
-    rsync -avz ~/.config/nvim/init.vim .config_macos/nvim/init.vim
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    /opt/homebrew/bin/rsync -avz ~/.zshrc .zshrc_macos
+    /opt/homebrew/bin/rsync -avz ~/.antigen .antigen_macos
+    /opt/homebrew/bin/rsync -avz ~/.config/nvim/init.vim .config_macos/nvim/init.vim
+elif [[ "$OSTYPE" == "FreeBSD" ]]; then
     # FreeBSD
-    rsync -avz ~/.zshrc .zshrc_freebsd
-    rsync -avz ~/.antigen .antigen_freebsd
-    rsync -avz ~/.config/konsolerc konsolerc_freebsd
-    rsync -avz ~/.config/yakuakerc yakuakerc_freebsd
-    rsync -avz ~/.config/nvim/init.vim .config_freebsd/nvim/init.vim
+    /usr/local/bin/rsync -avz ~/.zshrc .zshrc_freebsd
+    /usr/local/bin/rsync -avz ~/.antigen .antigen_freebsd
+    /usr/local/bin/rsync -avz ~/.config/konsolerc konsolerc_freebsd
+    /usr/local/bin/rsync -avz ~/.config/yakuakerc yakuakerc_freebsd
+    /usr/local/bin/rsync -avz ~/.config/nvim/init.vim .config_freebsd/nvim/init.vim
 else
     echo "Unsupported operating system: $OSTYPE"
     exit 1
